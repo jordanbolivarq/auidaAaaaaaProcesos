a36c529644786e3454ae2a89c4bef0585a435710
jordanbolivarq <60189623+jordanbolivarq@users.noreply.github.com>
2021-11-01 21:40:13 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,9 @@
-﻿using System;
+using System;
 
 namespace TrabajoFinalProcesos
 {
     class Program
     {
-        static float[][] multiplyMatrix(float a[][], float b[][]) {
-          int ar = a.length; // Matrix a rows
-          int ac = a[0].length;  // Matrix a columns
-          int br = b.length; // Matrix b rows
-          int bc = b[0].length; // Matrix b columns
-          
-          float p[ar][bc] = new float[ar][bc]; // Product
-          for(int row = 0; row < ar; row++) {
-            for(int col = 0; col < bc.lenght) {
-               for(int i = 0; i < ac; i++) {
-                  p[row][column] += a[row][i] * b[row][i] 
-               }
-            }
-          }
-          
-          return p;
-        }
         static void Main()
         {
             //Simulacion 
@@ -90,54 +73,85 @@ namespace TrabajoFinalProcesos
 
             ////////////////////////////////////////////////////////////////////////
 
+            /*
             //Probabilidad 
             Console.WriteLine("\n\n Probabilidad \n");
 
-            float t[4][4] = { // Transition Matrix
-              // Col 1  Col 2   Col 3   Col 4
-              { 0.166f, 0.333f, 0.333f, 0.166f }, // Row 1
-              //[0,0]  [0,1]    [0,2]   [0,3]
-              { 0.166f, 0.166f, 0.333f, 0.333f }, // Row 2
-              //[1,0]  [1,1]    [1,2]   [1,3]
-              { 0.333f, 0.166f, 0.166f, 0.333f }, // Row 3
-              //[2,0]  [2,1]    [2,2]   [2,3]
-              { 0.333f, 0.333f, 0.166f, 0.166f }  // Row 4
-              //[3,0]  [3,1]    [3,2]   [3,3]
-            };
-          
-            float r[4][4] = { // Result Matrix
-                // Col 1  Col 2   Col 3   Col 4
-                { 0.166f, 0.333f, 0.333f, 0.166f }, // Row 1
-                //[0,0]  [0,1]    [0,2]   [0,3]
-                { 0.166f, 0.166f, 0.333f, 0.333f }, // Row 2
-                //[1,0]  [1,1]    [1,2]   [1,3]
-                { 0.333f, 0.166f, 0.166f, 0.333f }, // Row 3
-                //[2,0]  [2,1]    [2,2]   [2,3]
-                { 0.333f, 0.333f, 0.166f, 0.166f }  // Row 4
-                //[3,0]  [3,1]    [3,2]   [3,3]
-              };
+            float[,] transicion = new float[4, 4];
 
-            float initialProb[4] = { 0.166f, 0.333f, 0.333f, 0.166f };
-          
-            float r[4] = { 4.2f, 2.2f, 6f, 9f }; // Retribution array
-            
+            //fila 1
+            transicion[0, 0] = 0.166f;
+            transicion[0, 1] = 0.333f;
+            transicion[0, 2] = 0.333f;
+            transicion[0, 3] = 0.166f;
+            //fila 2
+            transicion[1, 0] = 0.166f;
+            transicion[1, 1] = 0.166f;
+            transicion[1, 2] = 0.333f;
+            transicion[1, 3] = 0.333f;
+            //fila 3
+            transicion[2, 0] = 0.333f;
+            transicion[2, 1] = 0.166f;
+            transicion[2, 2] = 0.166f;
+            transicion[2, 3] = 0.333f;
+            //fila 4
+            transicion[3, 0] = 0.333f;
+            transicion[3, 1] = 0.333f;
+            transicion[3, 2] = 0.166f;
+            transicion[3, 3] = 0.166f;
+
+            Console.WriteLine(transicion[0, 0] + " " + transicion[0, 1] + " " + transicion[0, 2] + " " + transicion[0, 3] + " ");
+            Console.WriteLine(transicion[1, 0] + " " + transicion[1, 1] + " " + transicion[1, 2] + " " + transicion[1, 3] + " ");
+            Console.WriteLine(transicion[2, 0] + " " + transicion[2, 1] + " " + transicion[2, 2] + " " + transicion[2, 3] + " ");
+            Console.WriteLine(transicion[3, 0] + " " + transicion[3, 1] + " " + transicion[3, 2] + " " + transicion[3, 3] + " \n\n");
+
+            float[,] probIniciales = new float[1, 4];
+
+            //fila 1
+            probIniciales[0, 0] = 0.166f;
+            probIniciales[0, 1] = 0.333f;
+            probIniciales[0, 2] = 0.333f;
+            probIniciales[0, 3] = 0.166f;
+
+
+            float[,] retribucion = new float[1, 4];
+
+            //fila 1
+            retribucion[0, 0] = 4.2f;
+            retribucion[0, 1] = 2.2f;
+            retribucion[0, 2] = 6f;
+            retribucion[0, 3] = 9f;
 
             float[,] transicionN;
             float[,] transicionTemp;
             float[,] probNLanzamientos;
 
             //transicion a la n
-            float tn[4][4] = new float[4][4];
-            for(int i = 0; i < cantidadTiros; i++) {
-              tn += multiplyMatrix(t, t);// accumulate result
-            }
-            float res = multiplyMatrix(initialProb, tn);
+            transicionTemp = new float[transicion.GetLength(0), transicion.GetLength(1)];
+            transicionTemp = transicion;
 
+            for (int i = 0; i < cantidadTiros; i++)//n veces
+            {
+                //transicionTemp * transicionTemp
+                for (int j = 0; j < transicion.GetLength(1)/*columnas matriz b; j++)
+                {
+                    for (int q = 0; q < transicionTemp.GetLength(0)/*filas matriz a; q++)
+                    {
+                        float sum = 0;
+                        for (int k = 0; k < transicionTemp.GetLength(1)/*columna matriz a; k++)
+                        {
+                            sum = transicionTemp[q, k] * transicion[q, k];
+                        }
+                        transicionTemp[q, j] = sum;
+                    }
+                }
+            }
 
             Console.WriteLine(transicionTemp[0, 0] + " " + transicionTemp[0, 1] + " " + transicionTemp[0, 2] + " " + transicionTemp[0, 3] + " ");
             Console.WriteLine(transicionTemp[1, 0] + " " + transicionTemp[1, 1] + " " + transicionTemp[1, 2] + " " + transicionTemp[1, 3] + " ");
             Console.WriteLine(transicionTemp[2, 0] + " " + transicionTemp[2, 1] + " " + transicionTemp[2, 2] + " " + transicionTemp[2, 3] + " ");
             Console.WriteLine(transicionTemp[3, 0] + " " + transicionTemp[3, 1] + " " + transicionTemp[3, 2] + " " + transicionTemp[3, 3] + " ");
+            */
         }
     }
 }
